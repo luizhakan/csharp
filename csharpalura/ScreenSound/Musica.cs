212317ee562df899f247e4089877eed8f27e603f
luizhakan <luizhakan2@protonmail.com>
2023-12-26 20:00:42 +0000

    Métodos de acesso e propriedades

--- csharpalura/ScreenSound/Musica.cs
+++ csharpalura/ScreenSound/Musica.cs
@@ -1,15 +1,25 @@
 class Musica
 {
-    public string nome;
-    public string artista;
-    public int duracao;
-    public bool disponivel;
+    public string Nome {get; set;}
+    public string Artista {get; set;}
+    public int Duracao {get; set;}
+    public bool Disponivel {get; set;}
+    public string DescricaoResumida => $"A música {Nome} pertence à {Artista}";
+    // public int Somar(int a, int b) => a + b;
+
+    public void EscreveDisponivel(bool value){
+        Disponivel = value;
+    }
+
+    public bool LeDisponivel(){
+        return Disponivel;
+    }
 
     public void ExibirFichaTecnica(){
-        Console.WriteLine($"\n{nome} - {artista}");
-        Console.WriteLine($"{duracao} min");
+        Console.WriteLine($"\n{Nome} - {Artista}");
+        Console.WriteLine($"{Duracao} min");
 
-        if (disponivel) {
+        if (Disponivel) {
             Console.WriteLine("Disponivel no plano");
         } else {
             Console.WriteLine("Adquira o plano");
--- csharpalura/ScreenSound/Program.cs
+++ csharpalura/ScreenSound/Program.cs
@@ -1,13 +1,24 @@
 ﻿Musica musica1 = new Musica();
-musica1.nome = "Girl from Ipanema";
-musica1.artista = "Frank Sinatra";
-musica1.duracao = 3;
-musica1.disponivel = true;
+musica1.Nome = "Girl from Ipanema";
+musica1.Artista = "Frank Sinatra";
+musica1.Duracao = 3;
+musica1.EscreveDisponivel(true);
 musica1.ExibirFichaTecnica();
 
 Musica musica2 = new Musica();
-musica2.nome = "Fly me to the moon";
-musica2.artista = "Frank Sinatra";
-musica2.duracao = 4;
-musica2.disponivel = false;
+musica2.Nome = "Fly me to the moon";
+musica2.Artista = "Frank Sinatra";
+musica2.Duracao = 4;
+Console.WriteLine(musica2.DescricaoResumida);
 musica2.ExibirFichaTecnica();
+
+// List<int> numeros = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+// List<int> numerosPares = numeros.FindAll(x => x % 2 == 0);
+
+// Console.WriteLine("Numeros pares:");
+// foreach (int num in numerosPares)
+// {
+//     Console.WriteLine(num);
+// }
+
+// Console.WriteLine(musica1.Somar(1, 2));
